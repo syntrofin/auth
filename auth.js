d6a91500c0a2e7a63cb24a69d9773028dc590cfe
syntrofin <ravigo@gmail.com>
2026-01-26 13:12:53 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,16 @@
 export default function handler(req, res) {
-  // CORRECT way to add CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "https://syntrofin.com");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  //CORS
+  const allowedOrigins = ['https://syntrofin.com', 'https://www.syntrofin.com'];
+  const origin = req.headers.origin;
 
-  // Handle preflight request
+  // If the requester is in our allowed list, permit them
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
